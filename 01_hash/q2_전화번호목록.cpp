0c22e355f24f58ec0d995342dea5984165fa5fd6
singyuKang <75622950+singyuKang@users.noreply.github.com>
2021-06-29 15:10:28 +0900

    q2_전화번호목록
    hello

--- 01_hash/q2_전화번호목록.cpp
+++ 01_hash/q2_전화번호목록.cpp
@@ -1,2 +1,12 @@
-#include <iostream>
-   
+def solution(phone_book):
+    answer = true
+    phone_book.sort()
+    print(phone_book)
+    for i in range(len(phone_book)-1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+            answer = false
+            break
+    return answer
+
+phone_book = ["12","123","1235","567","88"]
+print(solution(phone_book))
