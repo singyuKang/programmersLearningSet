ea9b76f35261b979076a1fc8ae53829ccb177952
강신규 <kangsingyu@gangsingyuui-MacBookAir.local>
2022-05-07 01:21:22 +0900

    DFS_BFS
    cording

---
+++ cording_test/DFS_BFS.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+n, m , v = map(int, input().split())
+graph = [[0]*(n+1) for k in range(n+1)]
+visited_dfs = [0]*(n+1)
+
+# print(graph)
+
+for i in range(n+1):
+    a,b = map(int, input().split())
+    graph[a][b] = graph[b][a] = 1
+
+def dfs(v):
+    visited_dfs[v] = 1
+    print(v, end=' ')
+
+    for i in range(n+1):
+        if visited_dfs[i] == 0 and graph[i][v]:
+            dfs(i)
+dfs(v)
+
+visited_bfs = [0]*(n+1)
+
+def bfs(v):
+    visited_bfs[v] = 1
+    que = deque()
+    que.append(v)
+
+    while que:
+        node = que.popleft()
+        print(node, end= ' ')
+        for i in range(n+1):
+            if visited_bfs[i] == 0 and graph[i][node]:
+                que.append(i)
+                visited_bfs[i] = 1
+bfs(v)
+
+
+
+
+
+
